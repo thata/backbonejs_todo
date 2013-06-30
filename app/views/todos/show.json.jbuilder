ead6f6af2def0c8655c750d1baf2f332bf5c345a
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-07-01 00:22:45 +0900

    add todo

---
+++ app/views/todos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo, :name, :done, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BackbonejsTodo::Application.routes.draw do
+  resources :todos
+
   get "hello/world"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
