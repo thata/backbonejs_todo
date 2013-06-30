9e8b4283fbbaa9187270b41c4717abcbc673be19
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2013-06-30 23:44:57 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackbonejsTodo::Application.config.secret_key_base = '3c36e4e7155ad7b667330a0725035cfe163bf222f8ae537c405e1ae2175126115d1c5632292c04342f1fb25fd1bf8f5ad1e7d37484c792be574b6b6c7e871762'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackbonejsTodo::Application.config.session_store :cookie_store, key: '_backbonejs_todo_session'
